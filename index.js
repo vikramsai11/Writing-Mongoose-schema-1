d9109295be29416bb68cea641e207e9b494962b6
vikramsai10 <vikramsaithota@gmail.com>
2025-02-24 18:19:07 +0530

    added to server

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+
+
+const DB_URL = 'mongodb+srv://vikramsaithotas86:vikramsai2006@assignment.d0bqd.mongodb.net/?retryWrites=true&w=majority&appName=assignment';
+
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
